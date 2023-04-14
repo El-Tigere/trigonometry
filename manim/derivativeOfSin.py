4ebefc0f317bb0c6d9a8ff4be5f4c01bb9f6abf8
El_Tigere <eltigere@web.de>
2023-04-14 23:03:57 +0200

    added proof for the derivative of sin

--- manim/derivativeOfSin.py
+++ manim/derivativeOfSin.py
@@ -5,6 +5,22 @@ class DerivSin(Scene):
         
         text = MathTex("\\frac{d}{dx}\\ sin(x)")
         
+        # TODO: use better animations
         self.play(Create(text))
         self.wait(1)
+        self.play(Transform(text, MathTex("^{lim}_{h\\rightarrow0}\\ \\frac{sin(x+h)-sin(x)}{h}")))
+        self.wait(1)
+        self.play(Transform(text, MathTex("^{lim}_{h\\rightarrow0}\\ \\frac{sin(x)cos(h)+cos(x)sin(h)-sin(x)}{h}")))
+        self.wait(1)
+        self.play(Transform(text, MathTex("^{lim}_{h\\rightarrow0}\\ \\frac{sin(x)(cos(h)-1)+cos(x)sin(h)}{h}")))
+        self.wait(1)
+        self.play(Transform(text, MathTex("^{lim}_{h\\rightarrow0}\\ \\frac{sin(x)(cos(h)-1)}{h}+\\frac{cos(x)sin(h)}{h}")))
+        self.wait(1)
+        self.play(Transform(text, MathTex("^{lim}_{h\\rightarrow0}\\ \\frac{sin(x)(cos(h)-1)}{h}+^{lim}_{h\\rightarrow0}\\ \\frac{cos(x)sin(h)}{h}")))
+        self.wait(1)
+        self.play(Transform(text, MathTex("0+cos(x)")))
+        self.wait(1)
+        self.play(Transform(text, MathTex("\\frac{d}{dx}\\ sin(x)=cos(x)")))
+        self.wait(1)
+        
         self.play(FadeOut(text))
